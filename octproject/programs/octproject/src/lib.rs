102d51465c224fef8988a054fe6e7301c6dd71fd
christine taylor <chris@snakecharmer.com>
2021-11-09 14:14:03 -0800

    Deploy to Solana Devnet

--- octproject/programs/octproject/src/lib.rs
+++ octproject/programs/octproject/src/lib.rs
@@ -1,6 +1,6 @@
 use anchor_lang::prelude::*;
 
-declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+declare_id!("A2myRDoA2PXrJUVmrCKFAUyimFDfVzkAZpXaQvkFncEz");
 
 #[program]
 pub mod octproject {
